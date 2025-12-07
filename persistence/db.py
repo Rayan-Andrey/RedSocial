6f24da2d4641225ce2c24ef5e41f179171ce4728
Rayan-Anddrey <allanalmendroslikp@gmail.com>
2025-12-07 12:09:35 -0700

    alternar entre las bases de datos

--- persistence/db.py
+++ persistence/db.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+#comentarta cual base es la que se esta usando
+
 DATABASE_URL = "mysql+pymysql://root:password@localhost/red_social"
+#DATABASE_URL = "mysql+pymysql://root:Admin@localhost/red_social"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
